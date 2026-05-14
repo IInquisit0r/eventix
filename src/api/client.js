ed91bfeb40b586e49d35d438374adce97e22eeaa
1nquisit0r <d1m4z06@yandex.ru>
2026-05-14 15:19:32 +0300

    fix: api

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 const BASE_URL =
   import.meta.env.DEV
     ? "http://localhost:3001/api"
-    : "https://eventix-mrv6.onrender.com";
+    : "https://eventix-mrv6.onrender.com/api";
 
 export const apiRequest = async (endpoint) => {
   const res = await fetch(`${BASE_URL}${endpoint}`);
